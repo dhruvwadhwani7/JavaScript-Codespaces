d774cd87e8fa2f0618e0d9ef734caa3e1229070f
dhruvwadhwani7 <dhruvwadhwani77277@gmail.com>
2025-07-14 21:33:00 +0530

    JS strings basics

--- 02_basics/01_strings.js
+++ 02_basics/01_strings.js
@@ -1,8 +1,8 @@
 const myName = "Dhruv"
 const clickCount = 50 
 console.log(clickCount + " is the Value of clicks of person " + myName);
-
-console.log(`My name is ${myName} click count is ${clickCount}`);
+// this is string interpolation 
+console.log(`My name is ${myName.toUpperCase()} click count is ${clickCount}`);
 
 // String decalration using objcrts 
 const gameName = new String("Mario")
@@ -18,7 +18,52 @@ String {'Mario'}
 length: 5
 [[Prototype]]: String[[PrimitiveValue]]: "Mario"*/
 
-console.log(gameName[0]);
-console.log(gameName.slice(0,3));
+console.log(gameName[0]); // accesing the keys 
+console.log(gameName.slice(0,3)); // accessign the prototypes 
 
-// 7:06 minutues  --- Strings in javascript
+console.log(gameName.__proto__) // for seeign the prototypes
+console.log(gameName.toUpperCase()); //function
+console.log(gameName.length); // 5 
+console.log(gameName.charAt(2)) //r 
+console.log(gameName.charAt("M")) // wrong because the charAt function takes the input of number or index input and JS will convert this into the number 
+// "M" => NaN => 0 (for indexing)
+// so console.log(gameName.charAt("D")) => console.log(gameName.charAt(0))   hence ans M 
+
+console.log(gameName.charAt("4")) //0
+
+
+// for returning index 
+console.log(gameName.indexOf("r")) // 
+
+const newString = gameName.substring(1,4)
+console.log(newString)  
+
+console.log(gameName.split(''))
+//gets split into a array and separated by '' 
+
+
+// slicing negative  
+console.log(gameName.slice(-5,4)) //Mari
+console.log(gameName.split('').reverse().join('')) //reverses the string 
+
+    const newStringOne = "    #####Dhruv Wadhwani   "
+console.log(newStringOne)
+console.log(newStringOne.trim()) //removes the trailing white spaces
+
+
+// replace and includes
+const url = "https://github.com/dhruvwadhwani7/JavaScript-Codespaces"
+const url2 = "https://www.jetbrains.com/webstorm/download/download-thanks.html?platform=windows"
+
+console.log(url.replace("dhruv","hello"))
+console.log(url.includes("https")) //true
+console.log(url.startsWith("https://github")); //true
+console.log(url.startsWith("hello")); //false
+
+console.log(url2.replace("download","JS")) // replaces only first occurrences 
+console.log(url2.replace(/download/g, "JS")); // repalce all occurences using /g as global flag in JS
+
+//spliting 
+console.log(url.split("/"))
+console.log(url.split(" "))
+console.log(url.split("-"))
---
+++ 02_basics/02_numsAndMaths.js
@@ -0,0 +1,12 @@
+// creation of numbers
+const score = 100
+console.log(score)
+const balance = new Number(10000)
+console.log(balance)
+
+
+//method 
+console.log(balance.toString()) //converts to the string
+console.log(typeof balance.toString()) // returns string
+
+console.log(score.toExponential(2))  // output in exponentional scientific format 
