1a778e2fac1e20e34752379f57adafdb380e026d
dhruvwadhwani7 <dhruvwadhwani77277@gmail.com>
2025-07-19 22:15:18 +0530

    object advanced

---
+++ 03_basics/04_objects.js
@@ -0,0 +1,17 @@
+// const tinder = new Object()
+const tinder = {}
+tinder.id = "123abc"
+tinder.name = "Vedant"
+tinder.isLoggedIn = true
+console.log(tinder)
+
+const regularUser ={
+    email : "dhruv@gmail.com",
+    fullname : {
+        userfullname : {
+            firstname : "Dhruv",
+            lastname : "Wadhwani"
+        }
+    },
+} 
+// 4:08
