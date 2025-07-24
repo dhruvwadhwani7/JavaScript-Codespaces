ee6a30b8b42cce325587d78db1188117bdd8160f
dhruvwadhwani7 <dhruvwadhwani77277@gmail.com>
2025-07-24 23:46:24 +0530

    functions

---
+++ 04_basics/01_functions.js
@@ -0,0 +1,30 @@
+// function is package of a code --- in simpler terms 
+// you can take this package as a whole and use it in other or print 
+// it is taken in a manner of created copies (MULTIPLE)
+
+// console.log("Dhruv")
+// console.log("Wadhwani")
+
+// functions is used for these multiple usage codes or lines in your whole code
+function sayMyName() {
+    console.log("Dhruv")
+    console.log("Wadhwani")
+}
+// sayMyName // this is function refrence
+
+// console.log(sayMyName) // printing the refrence 
+
+// console.log(sayMyName()) //calling the function inside the console.log()
+// the output 
+// Dhruv
+// Wadhwani
+// undefined
+
+// sayMyName() //this is function  calling
+
+
+//ADDING TWO NUMBERS 
+function addTwoNumbers(num1,num2){
+    
+
+}
