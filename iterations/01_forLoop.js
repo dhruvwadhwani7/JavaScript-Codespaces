39e9a4d0896767c97248f1a33556741f4eb50714
dhruvwadhwani7 <dhruvwadhwani77277@gmail.com>
2025-08-01 20:23:52 +0530

    basic loops in JS break continue & for

--- iterations/01_forLoop.js
+++ iterations/01_forLoop.js
@@ -1,7 +1,40 @@
-for(let i = 1 ;i <= 8;i++){
-    const element = i  
-    if(element == 5){
-        console.log("best number is next")
+// for(let i = 1 ;i <= 8;i++){
+//     const element = i  
+//     if(element == 5){
+//         console.log("best number is next")
+//     }
+//     console.log(element)
+// }
+
+
+// TABLES 
+// for (let i = 1; i <= 3; i++) {
+//     console.log(`TABLE OF ${i}`)
+//     for (let j = 1; j <=10; j++) {
+//         // console.log(`outer loops --- ${j} for the inner loop ${i}`)
+//         console.log(`${i} * ${j} = ${i*j}`)
+//     }
+//     console.log("")
+// }
+
+//ARRAY 
+// const myArray = ["flash", "thor" , "batman"]
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+// }
+
+//BREAK AND CONTINUE
+// for (let  i = 1;  i <= 20;  i++) {
+//     console.log(`the value is ${i}`)
+//     if(i == 5){
+//         break
+//     }
+// }
+
+for (let  i = 1;  i <= 20;  i++) {
+    if(i == 5){
+        continue
     }
-    console.log(element)
+    console.log(`the value is ${i}`)
 }
