c7279b19fe27fddbb8c8c396619ee57e9ee10772
dhruvwadhwani7 <dhruvwadhwani77277@gmail.com>
2025-08-03 18:26:41 +0530

    while loops and do while loops

---
+++ iterations/02_while_doWhile_Loops.js
@@ -0,0 +1,26 @@
+let index = 1 
+while (index<10) {
+    console.log("*".repeat(index))
+    index += 2  
+}
+
+let myArray = ["thor", "flash" , "hero"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(myArray[arr])
+    arr++
+}
+
+
+let score = 1
+do {
+    console.log(score)
+    score ++ 
+} while (score<10);
+
+
+let score2 = 11
+do {
+    console.log(score2)
+    score2 ++ 
+} while (score2<=10);
